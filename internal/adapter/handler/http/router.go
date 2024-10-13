57edd831ba3731a3cf511c8363381cf941496129
Pawin Nakvisai <pawinnak2@gmail.com>
2024-10-13 14:46:41 +0700

    [ADD] Delete ingredient stock endpoint

--- internal/adapter/handler/http/router.go
+++ internal/adapter/handler/http/router.go
@@ -41,6 +41,7 @@ func NewRouter(a *fiber.App, ingredientHandler IngredientHandler, recipeHandler 
 			ingredient.Get("/getIngredientStockDetail", ingredientHandler.GetIngredientStockDetail)
 			ingredient.Delete("/deleteIngredientBatchNote", ingredientHandler.DeleteIngredientBatchNote)
 			ingredient.Delete("/deleteIngredient", ingredientHandler.DeleteIngredient)
+			ingredient.Delete("/deleteIngredientStock", ingredientHandler.DeleteIngredientStock)
 			ingredient.Post("/addIngredient", ingredientHandler.AddIngredient)
 			ingredient.Post("/addIngredientStock", ingredientHandler.AddIngredientStock)
 		}
