3598586af36d3f9808cf4ec2769cb1b3da744c61
Nattawat Teaiwtan <Nao.Nait@gmail.com>
2024-11-01 00:22:53 +0700

    [UPDATE] schema.prisma & GetAllNoti and CreateNoti endpoints

--- internal/core/domain/notification.go
+++ internal/core/domain/notification.go
@@ -1,11 +1,15 @@
 package domain
 
 type NotificationItem struct {
-	Title     string `json:"title"`
-	Message   string `json:"message"`
-	CreatedAt string `json:"created_at"`
-	IsRead    bool   `json:"is_read"`
-	NotiType  string `json:"noti_type"`
+	NotiID       string `json:"noti_id"`
+	Title        string `json:"title"`
+	Message      string `json:"message"`
+	CreatedAt    string `json:"created_at"`
+	IsRead       bool   `json:"is_read"`
+	NotiType     string `json:"noti_type"`
+	ItemID       string `json:"item_id"`
+	ItemName     string `json:"item_name"`
+	NotiItemType string `json:"noti_item_type"`
 }
 
 type NotificationList struct {
@@ -13,12 +17,15 @@ type NotificationList struct {
 }
 
 type CreateNotificationItem struct {
-	UserID      string `json:"user_id"`
-	ThaiTitle   string `json:"thai_title"`
-	EngTitle    string `json:"eng_title"`
-	ThaiMessage string `json:"thai_message"`
-	EngMessage  string `json:"eng_message"`
-	CreatedAt   string `json:"created_at"`
-	IsRead      bool   `json:"is_read"`
-	NotiType    string `json:"noti_type"`
+	UserID       string `json:"user_id"`
+	ThaiTitle    string `json:"thai_title"`
+	EngTitle     string `json:"eng_title"`
+	ThaiMessage  string `json:"thai_message"`
+	EngMessage   string `json:"eng_message"`
+	CreatedAt    string `json:"created_at"`
+	IsRead       bool   `json:"is_read"`
+	NotiType     string `json:"noti_type"`
+	ItemID       string `json:"item_id"`
+	ItemName     string `json:"item_name"`
+	NotiItemType string `json:"noti_item_type"`
 }
